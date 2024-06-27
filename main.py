78fbb3fa194c6b8ea811ba6425faf178710e16ad
hockeyrf <55231990+hockeyrf@users.noreply.github.com>
2024-06-27 23:52:37 +0300

    add record
    add record function has added

--- main.py
+++ main.py
@@ -10,15 +10,23 @@ root = Tk()  # создаем корневой объект - окно
 root.title("Приложение на Tkinter")  # устанавливаем заголовок окна
 root.geometry("1024x768")  # устанавливаем размеры окна
 
+
 def warning_dialog(text: str):
     messagebox.showinfo('Внимание', text)
 
 
 def read_all():
     phone_book.clear()
-    with open(phonebook_file_name, 'r', encoding='utf-8') as phb:
+    with open(phonebook_file_name, 'a+', encoding='utf-8') as phb:
         for line in phb:
             phone_book.append(dict(zip(fields, line.split(','))))
+
+
+def write_record(last_name: str, name: str, phone: str, desc: str):
+    with open(phonebook_file_name, 'a', encoding='utf-8') as phb:
+        phone_book.append(dict(zip(fields, [last_name, name, phone, desc])))
+        phb.write(f'{last_name},{name},{phone},{desc}\n')
+    update_listbox(records)
 
 
 def check_record_exist(text: str) -> bool:
@@ -26,12 +34,6 @@ def check_record_exist(text: str) -> bool:
         if text in phb.read():
             return True
     return False
-
-
-def write_record(last_name: str, name: str, phone: str, desc: str):
-    with open(phonebook_file_name, 'a', encoding='utf-8') as phb:
-        phone_book.append(dict(zip(fields, [last_name, name, phone, desc])))
-        phb.write(f'{last_name},{name},{phone},{desc}\n')
 
 
 def add_record(last_name: str, name: str, phone: str, desc: str):
@@ -45,7 +47,9 @@ def add_record(last_name: str, name: str, phone: str, desc: str):
 def update_listbox(records: Listbox):
     records.delete(0, END)
     for item in phone_book:
-        records.insert(item)
+        print(item)
+        # records.insert(f'{item[0]}\n {item[1]}\n {item[2]}\n {item[3]}')
+        records.insert(END, f"{item.get('Фамилия')}\n {item.get('Имя')}\n {item.get('Телефон')}\n {item.get('Описание')}")
 
 
 for column in range(4):
@@ -54,24 +58,26 @@ for row in range(5):
     root.rowconfigure(index=row, weight=1)
 
 
-ttk.Label(text='Фамилия').grid(row=0, column=0)
-last_name = ttk.Entry()
-last_name.grid(row=0, column=1)
+def create_frame(label_text):
+    frame = ttk.Frame(borderwidth=1, relief=SOLID, padding=[8, 10])
+    label = ttk.Label(frame, text=label_text)
+    label.pack(anchor=NW)
+    entry = ttk.Entry(frame)
+    entry.pack(anchor=NW)
+    return frame, entry
 
 
-ttk.Label(text='Имя').grid(row=1, column=0)
-name = ttk.Entry()
-name.grid(row=1, column=1)
+last_name_frame, last_name = create_frame("Фамилия")
+last_name_frame.grid(row=0, column=0)
 
+name_frame, name = create_frame("Имя")
+name_frame.grid(row=1, column=0)
 
-ttk.Label(text='Телефон').grid(row=2, column=0)
-phone = ttk.Entry()
-phone.grid(row=2, column=1)
+phone_frame, phone = create_frame("Телефон")
+phone_frame.grid(row=2, column=0)
 
-
-ttk.Label(text='Описание').grid(row=3, column=0)
-description = ttk.Entry()
-description.grid(row=3, column=1)
+desc_frame, description = create_frame("Описание")
+desc_frame.grid(row=3, column=0)
 
 add_rec_butt = ttk.Button(text='Добавить', command=lambda: add_record(last_name.get(), name.get(), phone.get(), description.get()))
 add_rec_butt.grid(row=4, column=0)
@@ -85,6 +91,10 @@ add_new_button = ttk.Button(text='Обновить', command=lambda: update_list
 add_new_button.grid(row=1, column=2)
 
 records = Listbox()
-records.grid(row=0, column=2, rowspan=3, sticky=NSEW)
+records.grid(row=0, column=1, rowspan=5, sticky=NSEW)
+
+
+read_all()
+update_listbox(records)
 
 root.mainloop()
